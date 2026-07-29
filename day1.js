ee3563b25c9f8dcc7cf639ef22d900dce8e68c32
Shivam Singh <singhshivam.ss.2101@gmail.com>
2026-07-29 09:50:01 +0530

    initial commit

--- day1.js
+++ day1.js
@@ -1,5 +1,53 @@
-const s={
-    name:"vikas",
-    age:27
+console.log("hello")
+
+
+//variable
+
+
+var name="shivaay"
+console.log(name);
+name="amit"
+console.log(name);
+var name="riya"
+console.log(name);
+
+
+
+//var=redeclartion and update
+//let
+
+
+
+let a=30;
+console.log(a);
+//let=no redecclaration 
+//const
+const a="india";
+console.log(a);
+a="usa";
+console.log(a);
+
+
+
+//object=key:value
+
+
+
+let s={
+    name:"shivaay",
+    age:"18"
 };
-console.log(s)
+console.log(s);
+const s={
+    name:"shivaay",
+    age:"19"
+};
+console.log(s);
+s.name="amit";
+console.log(s);
+s.city="ghaziabad";
+console.log(s);
+//array
+const f=["apple","banana"];
+f.push["mango"];
+f[0]="abc";
