8aed423e3121b1b4d6a42c86aa831382a358c2d0
Shivam Singh <singhshivam.ss.2101@gmail.com>
2026-07-29 10:17:35 +0530

    initial commit

---
+++ error.js
@@ -0,0 +1,14 @@
+//type error
+
+console.log("h"   //syntax error
+
+console.log(name);
+
+const num=10;
+num=20;
+
+const arr=new Array(-5);
+
+decodeURL("%");
+
+
---
+++ template.js
@@ -0,0 +1,8 @@
+// template literals
+
+let name="vikas";
+let age=27;
+let city="lucknow";
+
+console.log("name is :" +name);
+console.log(`name is : ${name}`);
