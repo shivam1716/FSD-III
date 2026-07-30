4180903bc3f0cb1a3d1fbdf2e0ed31b8c1ee7988
Shivam Singh <singhshivam.ss.2101@gmail.com>
2026-07-30 11:08:49 +0530

    initial commit

---
+++ numbermethod.js
@@ -0,0 +1,5 @@
+let num=125.7667;
+console.log(num.toFixed(2));
+console.log(num.toPrecision())   //give precised output based on user desire i.e. if given 5 will will output of five digits
+console.log(Number.isInteger(num));
+console.log(num.parseInt());
---
+++ stringmethod.js
@@ -0,0 +1,8 @@
+let text="javascript programming";
+
+console.log(text.length);
+console.log(text.toUpperCase());
+console.log(text.includes("hello"));
+console.log(text.startsWith("ja"));
+console.log(text.slice(0));
+console.log(text.substring());
